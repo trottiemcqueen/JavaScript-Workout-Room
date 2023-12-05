fbfd3d384466cbadc8f9134267f567474e5e2331
Trottie McQueen <mcqueen.trottie007@gmail.com>
2023-12-05 01:52:43 -0500

    updated file with sample code added js and html file

--- Basics/booleansAndIfStatements.js
+++ Basics/booleansAndIfStatements.js
@@ -1,1 +1,18 @@
 
+console.log(typeof 'true');
+console.log(typeof true);
+
+      //Comparison operators * lower priority than Math operators *
+      console.log(3 > 5);
+      console.log(5 == '5.00');
+      console.log(5 === '5.00');
+    //  (7 > second)  (first)
+      console.log(7 > 12 - 4);
+
+// IF STATEMENTS
+
+  if (false) {
+    console.log('hello')
+  }else {
+    console.log('see you later')
+  };
