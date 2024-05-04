173bd3386780123a96c7b146a9d69ef68c0e86a1
Trottie McQueen <mcqueen.trottie007@gmail.com>
2024-05-04 15:30:36 -0400

    finished datastructureArrayBuild troubleshooting breaking and fixing

--- Basics/ArrayMethods/datastructureArrayBuild.js
+++ Basics/ArrayMethods/datastructureArrayBuild.js
@@ -20,7 +20,22 @@ class MyArray {
     const lastItem = this.data[this.length-1];
     delete this.data[this.length-1];
     this.length--;
+    return lastItem;
+  }
+
+  delete(index) {
+    const item = this.data[index];
+    this.shiftItems(index);
     return item;
+  }
+
+  shiftItems(index) {
+    for (let i = index; i < this.length - 1; i++) 
+    {
+      this.data[i] = this.data[i+1];
+    }
+    delete this.data[this.length-1]
+    this.length--;
   }
 }
 
@@ -29,6 +44,8 @@ const newArray = new MyArray();
 newArray.push('hi');
 newArray.push('friend');
 newArray.push('!');
-newArray.pop();
+newArray.delete(0);
+newArray.push('are');
+newArray.push('nice');
 
 console.log(newArray);
