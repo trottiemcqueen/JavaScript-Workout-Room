63fb8ab8a1d7b87e772bbeea797504339b10c210
Trottie McQueen <mcqueen.trottie007@gmail.com>
2024-05-14 22:14:58 -0400

    figured out hash functionfor print the first recurring character

--- Basics/DataStructuresHashTables/ImplementHashTable.js
+++ Basics/DataStructuresHashTables/ImplementHashTable.js
@@ -1,1 +1,29 @@
 // Hash tables are great when you want quick access to certain values
+
+// Return the first recurring item
+function firstRecurringCharacter(input) {
+  for (let i = 0; i < input.length; i++) {
+    for (let j = i + 1; j < input.length; j++) {
+      if (input[i] === input[j]) {
+        return input[i];
+      }
+    }
+  }
+  return undefined;
+} //O(n^2)
+
+function firstRecurringCharacter2(input) {
+  let map = {};
+  for (let i = 0; i < input.length; i++) {
+    console.log(map[input[i]]);
+    if (map[input[i]] !== undefined) {
+      return input[i]
+    } else {
+      map[input[i]] = i
+    }
+    console.log(map)
+  }
+  
+  return undefined;
+}
+firstRecurringCharacter2([2,5,1,2,3,5,1,2,4])
