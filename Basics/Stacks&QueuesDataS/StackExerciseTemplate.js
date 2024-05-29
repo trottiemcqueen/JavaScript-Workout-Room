d6b8955c9cca9167dbc00013e0e44e4ad5a12dd5
Trottie McQueen <mcqueen.trottie007@gmail.com>
2024-05-29 17:51:30 -0400

    added a exercise solution 1

--- Basics/Stacks&QueuesDataS/StackExerciseTemplate.js
+++ Basics/Stacks&QueuesDataS/StackExerciseTemplate.js
@@ -14,7 +14,7 @@
 
 
 //Stack exercise template
-class Node {
+/* class Node {
     constructor(value) {
         this.value = value;
         this.next = null;
@@ -38,14 +38,68 @@ class Stack {
     }
 
     //isEmpty
-}
+//}
 
-const myStack = new Stack();
+//const myStack = new Stack();
 
 //Browser history:
 
 //Discord (push)
 //StackOverflow (pop)
-//Google (peek)
+//Google (peek)   */
 
+//Exercise solution 1
 
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class Stack {
+  constructor() {
+    this.top = null;
+    this.bottom = null;
+    this.length = 0;
+  }
+  peek() {
+    return this.top;
+  }
+  push(value) {
+    const newNode = new Node(value);
+    if (this.length === 0) {
+      this.top = newNode;
+      this.bottom = newNode;
+    } else {
+      const holdingPointer = this.top;
+      this.top = newNode;
+      this.top.next = holdingPointer;
+    }
+    this.length++;
+    return this;
+  }
+  pop() {
+    if (!this.top) {
+      return null;
+    }
+    if (this.top === this.bottom) {
+      this.bottom = null;
+    }
+    // const holdingPointer = this.top;
+    this.top = this.top.next;
+    this.length--;
+    return this;
+  }
+  //isEmpty
+}
+
+const myStack = new Stack();
+console.log(myStack.push("google"));
+console.log(myStack.push("google2"));
+console.log(myStack.peek());
+console.log(myStack.pop());
+console.log(myStack.pop());
+//Discord
+//ZTM
+//google
