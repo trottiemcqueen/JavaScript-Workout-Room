c516a60360a928dffdd37436292c32ffb04704a4
Trottie McQueen <mcqueen.trottie007@gmail.com>
2024-06-23 17:42:55 -0400

    updating the queueImplementation

--- Basics/Stacks&QueuesDataS/queueImplementation.js
+++ Basics/Stacks&QueuesDataS/queueImplementation.js
@@ -13,12 +13,30 @@ class Queue {
   }
 
 peek() {
-
+  return this.first;
 }
 enqueue(value) {
+  const newNode = new Node(value);
+  if (this.length === 0) {
+    this.first = newNode;
+    this.last = newNode;
 
+  } else {
+    this.last.next = newNode;
+    this.last = newNode;
+  }
+  this.length++;
+  return this;
 }
 dequeue() {
 
+  }
 }
-}
+
+const myQueue = new Queue();
+myQueue.enqueue('Joy');
+
+//Joy
+//Matt
+//Pavel
+//Samir
